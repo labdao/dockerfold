edd2b4c89dd3231fe7548c92a64af3c1cd10df59
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2022-09-12 14:58:32 +0200

    updating colabfold

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys 
+import os
+
+def predict(args):
+    return(output_path)
+
+def parse_arguments_main(): 
+    return(args)
+
+if __name__ == "__main__":
+    args = parse_arguments_main()
+    print("printing arguments:")
+    print("####################")
+    print(args)
+    print("####################")
+    print("running prediction:")
+    predict(args)
